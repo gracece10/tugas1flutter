e0e5eff8c06f3bfad70893d5690849b69cb34637
gracece10 <gresce10@gmail.com>
2023-10-03 12:06:05 +0800

    hmm cape deh

--- bin/null.dart
+++ bin/null.dart
@@ -1,22 +1,15 @@
 void main(List<String> args) {
   //Null merupakan tipe data representasi data kosong
   int number1;
-<<<<<<< HEAD
+
   number1 = 10; // bisa
   //number1 = null; // Error 'int' is not nullable
 
   int? number2;
   number2 = null; //bisa
 
-  print(number1); //Error int number1;
-=======
-  number1 = 10;
-  //number1 = null; // Error 'int' is not nullable
+  //print(number1); //Error int number1;
 
-  int? number2;
-  number2 = null;
-
-  print(number1); //Error
->>>>>>> b94f0c32ecab69f7aa98c7f144bce7c67e4a88b3
+  //print(number1); //Error
   print(number2); //Null
 }
