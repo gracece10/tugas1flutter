fba9ca500f5ae5d5b82244b3dfff9b1042d34c98
gracece10 <gresce10@gmail.com>
2023-10-03 11:15:24 +0800

    Fixed the 99th bug... for real this time!

--- bin/null.dart
+++ bin/null.dart
@@ -1,12 +1,12 @@
 void main(List<String> args) {
   //Null merupakan tipe data representasi data kosong
   int number1;
-  number1 = 10;
+  number1 = 10; // bisa
   //number1 = null; // Error 'int' is not nullable
 
   int? number2;
-  number2 = null;
+  number2 = null; //bisa
 
-  print(number1); //Error
+  print(number1); //Error int number1;
   print(number2); //Null
 }
